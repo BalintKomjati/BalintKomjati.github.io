416efc0a59e8a38423479d5cde6f4d83bb028d82
Balint Komjati <bkomjati@gmail.com>
2023-06-10 13:17:14 +0200

    Decreased hover fire delay

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-/*
-* Template Name: MyPortfolio
-* Template URL: https://bootstrapmade.com/myportfolio-bootstrap-portfolio-website-template/
-* License: https://bootstrapmade.com/license/
-*/
 
 (function ($) {
   "use strict";
